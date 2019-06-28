caedc7fa9b381cd5ba6d0cc792a5581896087c9d
haachicanoy <harold22010@gmail.com>
2019-06-28 10:00:29 -0500

    Improving Beta regression models

--- codes/02_1_SDynamics.R
+++ codes/02_1_SDynamics.R
@@ -25,73 +25,80 @@ sfs_chngs <- data.frame(Environment    = diff(sfs_chngs$Environment),
                         SFS_index      = diff(sfs_chngs$SFS_index))
 
 rolling_average_plot <- function(obs = 15){
-  plot(zoo::rollmean(x = sfs_index$Environment, k = obs), xlab = 'Countries position (high values = more sustainable)', ylab = 'Score values', ylim = c(0,1), ty = 'l') # Black
+  plot(x = zoo::rollmean(x = sfs_index$SFS_index, k = obs), y = zoo::rollmean(x = sfs_index$Environment, k = obs), xlab = 'SFS overall score', ylab = 'Score values', ylim = c(0,1), ty = 'l') # Black
   legend('bottomright', legend = c('Environment','Economic','Social','Food and nutrition'), col = 1:4, lty = 1)
-  lines(zoo::rollmean(x = sfs_index$Economic, k = obs), col = 2) # Red
-  lines(zoo::rollmean(x = sfs_index$Social, k = obs), col = 3) # Green
-  lines(zoo::rollmean(x = sfs_index$Food_nutrition, k = obs), col = 4) # Blue
+  lines(x = zoo::rollmean(x = sfs_index$SFS_index, k = obs), y = zoo::rollmean(x = sfs_index$Economic, k = obs), col = 2) # Red
+  lines(x = zoo::rollmean(x = sfs_index$SFS_index, k = obs), y = zoo::rollmean(x = sfs_index$Social, k = obs), col = 3) # Green
+  lines(x = zoo::rollmean(x = sfs_index$SFS_index, k = obs), y = zoo::rollmean(x = sfs_index$Food_nutrition, k = obs), col = 4) # Blue
 }
-rolling_average_plot(obs = 10)
+rolling_average_plot(obs = 15)
 
-
-pairs(sfs_index)
-pairs(sfs_chngs)
-cor(sfs_index[,-1], method = "spearman") %>% round(2)
-cor(sfs_chngs, method = "spearman") %>% round(2)
-cor(sfs_chngs[1:20,], method = "spearman")
-cor(sfs_chngs[21:40,], method = "spearman")
-cor(sfs_chngs[41:96,], method = "spearman")
-plot(sfs_chngs[,'Social'], sfs_index$SFS_index[-1], pch = 20)
-cor(sfs_chngs[,'Social'], sfs_index$SFS_index[-1], method = "spearman")
-
-lm_fit <- lm(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2, data = sfs_index)
+# Fitting a linear model (without intercept)
+lm_fit <- lm(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2 - 1, data = sfs_index)
 
 # Moving linear regression models
 linear_fits <- 1:73 %>% purrr::map(.f = function(i){
-  lm_fit  <- lm(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2, data = sfs_index[i:(24 + i),-1])
+  lm_fit  <- lm(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2 - 1, data = sfs_index[i:(24 + i),-1])
   results <- broom::tidy(lm_fit)
   results$order <- i
   return(results)
 }) %>% dplyr::bind_rows()
+linear_fits$sfs_mean <- 1:73 %>% purrr::map(.f = function(i){ sfs_index$SFS_index[i:(24 + i)] %>% mean}) %>% unlist %>% rep(., each = 10)
 linear_fits %>%
-  dplyr::filter(term %in% c('Environment', 'Economic', 'Social', 'Food_nutrition')) %>%
-  ggplot(aes(x = order, y = estimate, colour = term)) +
+  dplyr::filter(term %in% c('Environment', 'Economic', 'Social', 'Food_nutrition')) %>% # & p.value < 0.05
+  ggplot(aes(x = sfs_mean, y = estimate, colour = term)) +
+  scale_color_manual(values=c("red", "black", 'blue', "green")) +
   geom_line(size = 1.2) +
+  geom_point(size = 3) +
   geom_hline(yintercept = 0, col = 'red', lty = 2, size = 2)
 linear_fits %>%
   dplyr::filter(term %in% c('Environment:Economic','Environment:Social','Environment:Food_nutrition',
                             'Economic:Social','Economic:Food_nutrition','Social:Food_nutrition')) %>% # & p.value < 0.05
-  ggplot(aes(x = order, y = exp(estimate), colour = term)) +
+  ggplot(aes(x = sfs_mean, y = estimate, colour = term)) +
   geom_line(size = 1.2) +
+  geom_point(size = 3) +
   geom_hline(yintercept = 0, col = 'red', lty = 2, size = 2)
 
-bt_fit <- betareg::betareg(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2, link = 'logit', data = sfs_index)
+# Fitting a beta regression model (without intercept)
+bt_fit <- betareg::betareg(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2 - 1, link = 'logit', data = sfs_index)
 
 # Moving Beta regression models
 beta_fits <- 1:73 %>% purrr::map(.f = function(i){
-  bt_fit  <- betareg::betareg(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2, link = 'logit', data = sfs_index[i:(24 + i),-1])
+  bt_fit  <- betareg::betareg(SFS_index ~ Environment + Economic + Social + Food_nutrition - 1, link = 'logit', data = sfs_index[i:(24 + i),-1])
   results <- broom::tidy(bt_fit)
   results$order <- i
   return(results)
 }) %>% dplyr::bind_rows()
 beta_fits %>%
   dplyr::filter(term %in% c('Environment','Economic','Social','Food_nutrition')) %>% #  & p.value < 0.05
-  ggplot(aes(x = order, y = exp(estimate), colour = term)) +
+  ggplot(aes(x = order, y = estimate, colour = term)) +
+  scale_color_manual(values=c("red", "black", 'blue', "green")) + 
   geom_line(size = 1.2) +
+  geom_point(size = 3) +
   geom_hline(yintercept = 0, col = 'red', lty = 2, size = 2)
-beta_fits %>%
-  dplyr::filter(term %in% c('Environment:Economic','Environment:Social','Environment:Food_nutrition',
-                            'Economic:Social','Economic:Food_nutrition','Social:Food_nutrition')) %>% # & p.value < 0.05
-  ggplot(aes(x = order, y = exp(estimate), colour = term)) +
-  geom_line(size = 1.2) +
-  geom_hline(yintercept = 0, col = 'red', lty = 2, size = 2) +
-  ylim(0, 100)
 
+beta_fits2 <- 1:73 %>% purrr::map(.f = function(i){ # 1:73, 25 countries (24); # 1:48, 50 countries (49)
+  bt_fit  <- betareg::betareg(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2 - 1, link = 'logit', data = sfs_index[i:(24 + i),-1])
+  results <- broom::tidy(bt_fit)
+  results$order <- i
+  return(results)
+}) %>% dplyr::bind_rows()
+beta_fits2 %>%
+  dplyr::filter(term %in% c('Environment','Economic','Social','Food_nutrition',
+                            'Environment:Economic','Environment:Social','Environment:Food_nutrition',
+                            'Economic:Social','Economic:Food_nutrition','Social:Food_nutrition')) %>% #  & p.value < 0.05
+  ggplot(aes(x = order, y = estimate, colour = term)) +
+  geom_line(size = 1.2) +
+  geom_point(size = 3) +
+  geom_hline(yintercept = 0, col = 'red', lty = 2, size = 2)
 
 # Using local explanations
 # Linear model
-lm_fit <- lm(SFS_index ~ ., data = sfs_index[,-1])
+lm_fit <- lm(SFS_index ~ . - 1, data = sfs_index[,-1])
 exp_lm <- DALEX::explain(lm_fit, data = sfs_index[,2:5], y = sfs_index$SFS_index)
+
+# Beta model
+exp_bt <- DALEX::explain(bt_fit, data = sfs_index[,2:5], y = sfs_index$SFS_index)
 
 # Random forest
 rf_fit <- ranger::ranger(SFS_index ~ ., data = sfs_index[,-1], num.trees = 1000, importance = 'impurity')
@@ -115,7 +122,21 @@ plot(vi_lm, vi_rf)
 }) %>% dplyr::bind_rows() %>%
   dplyr::filter(variable %in% c('Environment', 'Economic', 'Social', 'Food_nutrition')) %>%
   ggplot(aes(x = observation, y = contribution, colour = variable)) +
-  geom_line()
+  scale_color_manual(values=c("red", "black", 'blue', "green")) + 
+  geom_line(size = 1.2) +
+  geom_point(size = 3)
+
+# Local explanation beta model
+1:nrow(sfs_index) %>% purrr::map(.f = function(i){
+  bd_bt <- breakDown::broken(model = exp_bt, new_observation = sfs_index[i,], data = sfs_index)
+  results <- data.frame(variable = bd_bt$variable_name, contribution = bd_bt$contribution, observation = i)
+  return(results)
+}) %>% dplyr::bind_rows() %>%
+  dplyr::filter(variable %in% c('Environment', 'Economic', 'Social', 'Food_nutrition')) %>%
+  ggplot(aes(x = observation, y = contribution, colour = variable)) +
+  scale_color_manual(values=c("red", "black", 'blue', "green")) + 
+  geom_line(size = 1.2) +
+  geom_point(size = 3)
 
 # Local explanation random forest
 1:nrow(sfs_index) %>% purrr::map(.f = function(i){
@@ -125,5 +146,229 @@ plot(vi_lm, vi_rf)
 }) %>% dplyr::bind_rows() %>%
   dplyr::filter(variable %in% c('Environment', 'Economic', 'Social', 'Food_nutrition')) %>%
   ggplot(aes(x = observation, y = contribution, colour = variable)) +
-  geom_line()
+  scale_color_manual(values=c("red", "black", 'blue', "green")) + 
+  geom_line(size = 1.2) +
+  geom_point(size = 3)
 
+# ------------------------------------------------------------- #
+# Creating quartile groups ordered by SFS overall score
+# ------------------------------------------------------------- #
+
+sfs_index2 <- sfs_index
+sfs_index2$Groups <- cut(sfs_index$SFS_index, breaks = quantile(sfs_index$SFS_index, probs = seq(0, 1, 0.25)))
+levels(sfs_index2$Groups) <- paste0('g',1:4)
+sfs_index2$Groups[1] <- 'g1'
+
+# Mean summarization
+summarySE <- function(data = NULL, measurevar, groupvars = NULL, na.rm = FALSE,
+                      conf.interval = .95, .drop = TRUE){
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = mean   (xx[[col]], na.rm=na.rm),
+                     sd   = sd     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+# Median summarization
+summarySEMedian <- function(data = NULL, measurevar, groupvars = NULL, na.rm = FALSE,
+                            conf.interval = .95, .drop = TRUE){
+  library(plyr)
+  
+  # New version of length which can handle NA's: if na.rm==T, don't count them
+  length2 <- function (x, na.rm=FALSE) {
+    if (na.rm) sum(!is.na(x))
+    else       length(x)
+  }
+  
+  # This does the summary. For each group's data frame, return a vector with
+  # N, mean, and sd
+  datac <- ddply(data, groupvars, .drop=.drop,
+                 .fun = function(xx, col) {
+                   c(N    = length2(xx[[col]], na.rm=na.rm),
+                     mean = median   (xx[[col]], na.rm=na.rm),
+                     sd   = mad     (xx[[col]], na.rm=na.rm)
+                   )
+                 },
+                 measurevar
+  )
+  
+  # Rename the "mean" column    
+  datac <- rename(datac, c("mean" = measurevar))
+  
+  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
+  
+  # Confidence interval multiplier for standard error
+  # Calculate t-statistic for confidence interval: 
+  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
+  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
+  datac$ci <- datac$se * ciMult
+  
+  return(datac)
+}
+
+# Mean summary
+env_dt <- summarySE(data = sfs_index2[,-1], measurevar = "Environment", groupvars = "Groups")
+eco_dt <- summarySE(data = sfs_index2[,-1], measurevar = "Economic", groupvars = "Groups")
+soc_dt <- summarySE(data = sfs_index2[,-1], measurevar = "Social", groupvars = "Groups")
+fnt_dt <- summarySE(data = sfs_index2[,-1], measurevar = "Food_nutrition", groupvars = "Groups")
+
+names(env_dt)[3] <- 'Values'
+names(eco_dt)[3] <- 'Values'
+names(soc_dt)[3] <- 'Values'
+names(fnt_dt)[3] <- 'Values'
+
+env_dt$Dimension <- "Environment"
+eco_dt$Dimension <- "Economic"
+soc_dt$Dimension <- "Social"
+fnt_dt$Dimension <- "Food_nutrition"
+
+final <- rbind(env_dt, eco_dt, soc_dt, fnt_dt)
+
+pd <- position_dodge(0.1)
+
+ggplot(final, aes(x = Groups, y = Values)) + 
+  geom_errorbar(aes(ymin = Values-ci, ymax = Values+ci), colour = "black", width = .1, position = pd) +
+  geom_line(position = pd) +
+  geom_point(position = pd, size = 3) +
+  facet_wrap(~Dimension) +
+  ylim(0, 1)
+rm(env_dt, eco_dt, soc_dt, fnt_dt, pd, final)
+
+# Median summary
+env_dt <- summarySEMedian(data = sfs_index2[,-1], measurevar = "Environment", groupvars = "Groups")
+eco_dt <- summarySEMedian(data = sfs_index2[,-1], measurevar = "Economic", groupvars = "Groups")
+soc_dt <- summarySEMedian(data = sfs_index2[,-1], measurevar = "Social", groupvars = "Groups")
+fnt_dt <- summarySEMedian(data = sfs_index2[,-1], measurevar = "Food_nutrition", groupvars = "Groups")
+
+names(env_dt)[3] <- 'Values'
+names(eco_dt)[3] <- 'Values'
+names(soc_dt)[3] <- 'Values'
+names(fnt_dt)[3] <- 'Values'
+
+env_dt$Dimension <- "Environment"
+eco_dt$Dimension <- "Economic"
+soc_dt$Dimension <- "Social"
+fnt_dt$Dimension <- "Food_nutrition"
+
+final2 <- rbind(env_dt, eco_dt, soc_dt, fnt_dt)
+
+pd <- position_dodge(0.1)
+
+ggplot(final2, aes(x = Groups, y = Values)) + 
+  geom_errorbar(aes(ymin = Values-ci, ymax = Values+ci), colour = "black", width = .1, position = pd) +
+  geom_line(position = pd) +
+  geom_point(position = pd, size = 3) +
+  facet_wrap(~Dimension) +
+  ylim(0, 1)
+rm(env_dt, eco_dt, soc_dt, fnt_dt, pd, final2)
+
+# Fitting Beta regression models
+models <- lapply(paste0('g',1:4), function(g){
+  db <- sfs_index2[sfs_index2$Groups == g,]
+  bt_fit <- betareg::betareg(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2 -1 , link = 'logit', data = db)
+  return(bt_fit)
+})
+models %>% purrr::map(.f = summary)
+
+# ------------------------------------------------------------- #
+# Creating quartile groups ordered by GDP per capita
+# ------------------------------------------------------------- #
+
+gdp <- read.csv(paste0(data_path,'/inputs_raw/gdp_per_capita_2010_us_dollars.csv'))
+sfs_index3 <- dplyr::left_join(sfs_index, data.frame(iso3c = gdp$iso3c, gdp = gdp %>% dplyr::select(X2004:X2015) %>% apply(., 1, median, na.rm = T)), by = "iso3c")
+sfs_index3 <- sfs_index3 %>% dplyr::arrange(gdp)
+
+sfs_index3$Groups <- cut(sfs_index3$gdp, breaks = quantile(sfs_index3$gdp, probs = seq(0, 1, 0.25)))
+levels(sfs_index3$Groups) <- paste0('g',1:4)
+sfs_index3$Groups[1] <- 'g1'
+
+# Mean summary
+env_dt <- summarySE(data = sfs_index3[,-1], measurevar = "Environment", groupvars = "Groups")
+eco_dt <- summarySE(data = sfs_index3[,-1], measurevar = "Economic", groupvars = "Groups")
+soc_dt <- summarySE(data = sfs_index3[,-1], measurevar = "Social", groupvars = "Groups")
+fnt_dt <- summarySE(data = sfs_index3[,-1], measurevar = "Food_nutrition", groupvars = "Groups")
+
+names(env_dt)[3] <- 'Values'
+names(eco_dt)[3] <- 'Values'
+names(soc_dt)[3] <- 'Values'
+names(fnt_dt)[3] <- 'Values'
+
+env_dt$Dimension <- "Environment"
+eco_dt$Dimension <- "Economic"
+soc_dt$Dimension <- "Social"
+fnt_dt$Dimension <- "Food_nutrition"
+
+final <- rbind(env_dt, eco_dt, soc_dt, fnt_dt)
+
+pd <- position_dodge(0.1)
+
+ggplot(final, aes(x = Groups, y = Values)) + 
+  geom_errorbar(aes(ymin = Values - ci, ymax = Values + ci), colour = "black", width = .1, position = pd) +
+  geom_line(position = pd) +
+  geom_point(position = pd, size = 3) +
+  facet_wrap(~Dimension) +
+  ylim(0, 1)
+rm(env_dt, eco_dt, soc_dt, fnt_dt, final, pd)
+
+# Median summary
+env_dt <- summarySEMedian(data = sfs_index3[,-1], measurevar = "Environment", groupvars = "Groups")
+eco_dt <- summarySEMedian(data = sfs_index3[,-1], measurevar = "Economic", groupvars = "Groups")
+soc_dt <- summarySEMedian(data = sfs_index3[,-1], measurevar = "Social", groupvars = "Groups")
+fnt_dt <- summarySEMedian(data = sfs_index3[,-1], measurevar = "Food_nutrition", groupvars = "Groups")
+
+names(env_dt)[3] <- 'Values'
+names(eco_dt)[3] <- 'Values'
+names(soc_dt)[3] <- 'Values'
+names(fnt_dt)[3] <- 'Values'
+
+env_dt$Dimension <- "Environment"
+eco_dt$Dimension <- "Economic"
+soc_dt$Dimension <- "Social"
+fnt_dt$Dimension <- "Food_nutrition"
+
+final2 <- rbind(env_dt, eco_dt, soc_dt, fnt_dt)
+
+pd <- position_dodge(0.1)
+
+ggplot(final2, aes(x = Groups, y = Values)) + 
+  geom_errorbar(aes(ymin = Values - ci, ymax = Values + ci), colour = "black", width = .1, position = pd) +
+  geom_line(position = pd) +
+  geom_point(position = pd, size = 3) +
+  facet_wrap(~Dimension) +
+  ylim(0, 1)
+rm(env_dt, eco_dt, soc_dt, fnt_dt, final2, pd)
+
+# Fitting Beta regression models
+models <- lapply(paste0('g',1:4), function(g){
+  db <- sfs_index3[sfs_index3$Groups == g,]
+  bt_fit <- betareg::betareg(SFS_index ~ (Environment + Economic + Social + Food_nutrition)^2 - 1, link = 'logit', data = db)
+  return(bt_fit)
+})
+models %>% purrr::map(.f = summary)
